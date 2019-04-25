855bce210b41e7c69eee9150cec6660ff0b9969b
Blake Redenius <blakeredenius@gmail.com>
2019-04-25 17:38:52 +1000

    Basic docs for MachineStatus.

--- src/main/java/me/xa5/simpletech/MachineStatus.java
+++ src/main/java/me/xa5/simpletech/MachineStatus.java
@@ -1,7 +1,18 @@
 package me.xa5.simpletech;
 
 public enum MachineStatus {
+    /**
+     * Machine has power, but there may not be an available output slot available, or another requirement was not met.
+     */
     IDLE,
+
+    /**
+     * Machine has power, and all requirements are met, and the machine is currently processing an input.
+     */
     PROCESSING,
+
+    /**
+     * Machine has no power and cannot run.
+     */
     OUT_OF_POWER,
 }
