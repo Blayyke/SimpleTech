00f86a7d46556f878b237a6c756bcf7d72b5c613
Blake Redenius <blakeredenius@gmail.com>
2019-04-25 17:36:41 +1000

    Drop machine contents on broken.

--- src/main/java/me/xa5/simpletech/blocks/machines/BaseMachineBlock.java
+++ src/main/java/me/xa5/simpletech/blocks/machines/BaseMachineBlock.java
@@ -5,8 +5,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateFactory;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
@@ -62,5 +65,32 @@ public abstract class BaseMachineBlock extends Block implements BlockEntityProvi
     @Override
     protected void appendProperties(StateFactory.Builder<Block, BlockState> stateBuilder) {
         stateBuilder.with(FACING);
+    }
+
+    @Override
+    public void onBlockRemoved(BlockState state, World world, BlockPos pos, BlockState blockState_2, boolean boolean_1) {
+        Machine machine = (Machine) world.getBlockEntity(pos);
+        for (int i = 0; i < machine.getInventorySize(); i++) {
+            ItemStack stack = machine.getInventory().getInvStack(i);
+            if (!stack.isEmpty()) {
+                dropStack(world, pos.getX(), pos.getY(), pos.getZ(), stack);
+            }
+        }
+    }
+
+    public static void dropStack(World world, double sourceX, double sourceY, double sourceZ, ItemStack stack) {
+        double entityWidth = (double) EntityType.ITEM.getWidth();
+        double double_5 = 1.0D - entityWidth;
+        double double_6 = entityWidth / 2.0D;
+        double x = Math.floor(sourceX) + world.random.nextDouble() * double_5 + double_6;
+        double y = Math.floor(sourceY) + world.random.nextDouble() * double_5;
+        double z = Math.floor(sourceZ) + world.random.nextDouble() * double_5 + double_6;
+
+        while (!stack.isEmpty()) {
+            ItemEntity entity = new ItemEntity(world, x, y, z, stack.split(world.random.nextInt(21) + 10));
+            entity.setVelocity(world.random.nextGaussian() * 0.05D, world.random.nextGaussian() * 0.05D + 0.2D, world.random.nextGaussian() * 0.05D);
+            world.spawnEntity(entity);
+        }
+
     }
 }
